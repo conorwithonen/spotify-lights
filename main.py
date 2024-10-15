ca83a58d750913814a0305e5b387acf32de04420
Conor Manning <conormanning13@gmail.com>
2024-10-15 14:46:10 -0500

    sloppy keyboardinterrupt handling

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import time
 import asyncio
 from src.spotify import get_currently_playing
 from src.scenes import update_light_scene
@@ -15,23 +14,25 @@ async def main():
     while True:
         if previous_song == current_song:
             # Song is still the same. Wait a bit and check again
-            time.sleep(WAIT_SECONDS)
+            try:
+                await asyncio.sleep(WAIT_SECONDS)
+            except asyncio.exceptions.CancelledError as e:
+                raise KeyboardInterrupt('No longer polling spotify')
+
             playback = get_currently_playing()
             current_song = playback.get('song')
 
         else:
-            # Change the song name
+            # Change the song name and lights
             print(f'Song changed to {current_song} by {playback.get("artist")}')
             previous_song = current_song
-
-            # Attempt to change scene based on new song
             await update_light_scene(playback)
 
 
 if __name__ == '__main__':
     try:
         asyncio.run(main())
-    except RuntimeError or KeyboardInterrupt as e:
-        print(f'Ending shopify light process: {e}')
+    except KeyboardInterrupt as e:
+        print(f'Manually ending shopify light process: {e}')
     except Exception as e:
-        print(f'Non-graceful exit: {e}')
+        print(f'Unexpected exception: {e}')
